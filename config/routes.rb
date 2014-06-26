7df65339e6f85b21adcc6a6cbf93dee032986f5e
irek <irek.skrobis@gmail.com>
2014-06-26 19:27:43 +0200

    routes refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,18 @@
 HappyCompany::Application.routes.draw do
-
   ActiveAdmin.routes(self)
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   devise_for :users
 
-  root to: "home#index"
+  root to: 'home#index'
 
   namespace :user do
-    root to: "home#index"
+    root to: 'home#index'
     resources :answers, only: [:index, :update]
-    resource :profile, only: [:edit, :update], controller: "profile"
+    resource :profile, only: [:edit, :update], controller: 'profile'
   end
 
   namespace :api do
-    resources :answers, only: [:show]
+    resources :answers, only: :show
   end
-
 end
