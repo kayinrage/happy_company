f570cb8c6d6253975bb97ca62a04a1e5d5fc466e
irek <irek.skrobis@gmail.com>
2014-07-27 10:51:00 +0200

    spec for Group model

---
+++ spec/models/group_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe Group do
+  it 'has valid factory' do
+    build(:group).should be_valid
+  end
+
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:parent_group_id) }
+  it { should validate_uniqueness_of(:name) }
+
+  context '#to_s' do
+    let(:group) { create(:parent_group, name: 'Selleo') }
+
+    it 'should returns name of group' do
+      expect(group.to_s).to eq 'Selleo'
+    end
+  end
+end
