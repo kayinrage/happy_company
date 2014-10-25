809ac37642dddab9e5a9cb459a4f11a8ead70ab1
irek <irek.skrobis@gmail.com>
2014-10-25 19:28:49 +0200

    User::HomeController - rspec

---
+++ spec/controllers/users/home_controller_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe User::HomeController do
+  render_views
+  include_context 'user signed in'
+
+  describe '#index' do
+    let(:call_request) { get :index }
+
+    it 'should create answer for current_user if answer does NOT exist for current day' do
+      expect { call_request }.to change { user.answers.count }.by 1
+    end
+
+    context 'answer exists for current day' do
+      before { user.answers.create(date: Date.today) }
+
+      it 'should NOT create answer' do
+        expect { call_request }.not_to change { user.answers.count }
+      end
+    end
+
+    context 'after call_request' do
+      before { call_request }
+
+      it { should render_template 'index' }
+    end
+  end
+end
