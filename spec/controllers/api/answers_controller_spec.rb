0d34ded2ebf03fd648ec3be2677d122278369104
irek <irek.skrobis@gmail.com>
2014-10-25 17:45:56 +0200

    Api::AnswersController - rspec

---
+++ spec/controllers/api/answers_controller_spec.rb
@@ -0,0 +1,69 @@
+require 'spec_helper'
+
+describe Api::AnswersController do
+  render_views
+
+  describe '#show' do
+    context 'with correct params' do
+      let(:params) { {id: '1', secret: 'secret', result: '3'} }
+      before { Answer.stub(:update_through_api).and_return(status: 'success', message: 'success') }
+
+      it 'should update answer' do
+        Answer.should_receive(:update_through_api).with(hash_including(params)).and_call_original
+        get :show, params
+      end
+
+      context 'after call request' do
+        before { get :show, params }
+
+        it 'should display correct flash message' do
+          expect(flash[:notice]).to eq 'success'
+        end
+
+        it 'should redirect to root_path' do
+          expect(response).to redirect_to root_path
+        end
+      end
+
+      context 'when user logged in' do
+        include_context 'user signed in'
+
+        it 'should redirect to user_root_path' do
+          get :show, params
+          expect(response).to redirect_to user_root_path
+        end
+      end
+    end
+  end
+
+  context 'with wrong params' do
+    let(:params) { {id: '1', secret: nil, result: '3'} }
+    before { Answer.stub(:update_through_api).and_return(status: 'fail', message: 'fail') }
+
+    it 'should not update answer' do
+      Answer.should_receive(:update_through_api).with(hash_including(params)).and_call_original
+      get :show, params
+    end
+
+    context 'after call request' do
+      before { get :show, params }
+
+      it 'should display correct flash message' do
+        expect(flash[:error]).to eq 'fail'
+      end
+
+      it 'should redirect to root_path' do
+        expect(response).to redirect_to root_path
+      end
+
+      context 'when user logged in' do
+        include_context 'user signed in'
+
+        it 'should redirect to user_root_path' do
+          get :show, params
+          expect(response).to redirect_to user_root_path
+        end
+      end
+    end
+  end
+end
